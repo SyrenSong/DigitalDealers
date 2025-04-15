c1b7f4ed1a5d5f68d402f22608e053bc4e4e0a04
Layla Musallam <163067730+SyrenSong@users.noreply.github.com>
2025-04-15 10:56:42 -0400

    added function to change text on full deck button

---
+++ DigitalDealers.js
@@ -0,0 +1,10 @@
+function checkDeckFull();
+{
+    if(document.getElementById("full-deck-button").value != "on") {
+      document.getElementById("full-deck-button").value="off";
+      document.getElementById("full-deck-button").innerHTML="Full Deck Off"
+    } else {
+      document.getElementById("full-deck-button").value="on";
+      document.getElementById("full-deck-button").innerHTML="Full Deck On"
+    }
+}
